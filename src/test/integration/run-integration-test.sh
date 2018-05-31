22305ef2abfd70ba3494adcf0be2ec3282db5ded
Lida He <lida.he@emc.com>
2018-05-31 16:40:38 -0400

    clean up cache
    Signed-off-by: Lida He <lida.he@emc.com>

--- src/test/integration/run-integration-test.sh
+++ src/test/integration/run-integration-test.sh
@@ -21,7 +21,10 @@ WAIT_RETRIES=${WAIT_RETRIES:-48}
 WAIT_SLEEP=${WAIT_SLEEP:-5}
 HTTP_OK=200
 
-ROOT_DIR=$PWD
+WORK_DIR=$PWD
+
+trap cleanup EXIT
+trap "exit 1" SIGTERM SIGHUP SIGINT SIGQUIT
 
 wait_for_service() {
     url=$1
@@ -38,7 +41,8 @@ wait_for_service() {
 }
 
 # Download flink
-FLINK_DIR=${ROOT_DIR}/flink-${FLINK_VERSION}
+cd $HOME/flink
+FLINK_DIR=$HOME/flink/flink-${FLINK_VERSION}
 FLINK_BINARY=flink-${FLINK_VERSION}-bin-hadoop28-scala_${SCALA_VERSION}.tgz
 wget --no-check-certificate https://archive.apache.org/dist/flink/flink-${FLINK_VERSION}/${FLINK_BINARY}
 tar zxvf $FLINK_BINARY
@@ -53,28 +57,29 @@ wait_for_service http://localhost:${FLINK_PORTAL_PORT}
 # Download and install Pravega
 # For the time being, use the Pravega submodule in Flink connector.
 # Eventually use a stable Pravega build that is compatible with Flink connector
-cd ${ROOT_DIR}/pravega
+cd ${WORK_DIR}/pravega
 #./gradlew startstandalone 2>&1 | tee /tmp/pravega.log &
 ./gradlew startstandalone > /dev/null 2>&1 &
 
 # wait for Pravega to start
 wait_for_service http://localhost:${PRAVEGA_REST_PORT}/v1/scopes
 
-cd ${ROOT_DIR}
+cd ${WORK_DIR}
 # flink connector artifact version
 commit_id=$(git log --format="%h" -n 1)
 count=$(git rev-list --count HEAD)
 version=${PRAVEGA_VERSION_PREFIX}-${count}.${commit_id}-SNAPSHOT
 
+ls -l $HOME/.m2/repository/io/pravega/pravega-connectors-flink_2.11/
+ls -l $FLINK_DIR/logs/*
+
 # Compile and run sample Flink application
-cd ${ROOT_DIR}
+cd ${WORK_DIR}
 git clone https://github.com//pravega/pravega-samples
-cd ${ROOT_DIR}/pravega-samples
+cd ${WORK_DIR}/pravega-samples
 git checkout develop
 sed -i '/connectorVersion/c\connectorVersion='${version}'' gradle.properties
 ./gradlew :flink-examples:installDist
-
-#rm -f ${FLINK_DIR}/log/* 
 
 # start ExactlyOnceWriter
 ${FLINK_DIR}/bin/flink run -c io.pravega.examples.flink.primer.process.ExactlyOnceWriter flink-examples/build/install/pravega-flink-examples/lib/pravega-flink-examples-0.3.0-SNAPSHOT-all.jar --controller tcp://localhost:${PRAVEGA_CONTROLLER_PORT} --scope myscope --stream mystream --exactlyonce true
@@ -82,16 +87,27 @@ ${FLINK_DIR}/bin/flink run -c io.pravega.examples.flink.primer.process.ExactlyOn
 # start ExactlyOnceChecker
 ${FLINK_DIR}/bin/flink run -c io.pravega.examples.flink.primer.process.ExactlyOnceChecker flink-examples/build/install/pravega-flink-examples/lib/pravega-flink-examples-0.3.0-SNAPSHOT-all.jar --controller tcp://localhost:${PRAVEGA_CONTROLLER_PORT} --scope myscope --stream mystream &
 
+# wait for 5 second to for job to register
+sleep 5
+
 set -x
-job_id=$((${FLINK_DIR}/bin/flink list)  | grep ExactlyOnce | awk '{print $4}')
+job_id=`${FLINK_DIR}/bin/flink list) | grep ExactlyOnceChecker | awk '{print $4}'`
 count=0
 ls -l ${FLINK_DIR}/log
-until grep -q "EXACTLY_ONCE" ${FLINK_DIR}/log/*; do
+until grep -q "EXACTLY_ONCE" ${FLINK_DIR}/log/*.out; do
     if [ $count -ge 24 ]; then
         ${FLINK_DIR}/bin/flink cancel $job_id
         exit 1
     fi
     count=$(($count+1))
     sleep ${WAIT_SLEEP}
-done
+fonm
 ${FLINK_DIR}/bin/flink stop $job_id
+
+cleanup() {
+    # clean up flink connector artifacts as $HOME/.m2 may be cacached.
+    rm -rf $HOME/.m2/repository/io/pravega/pravega-connectors-flink_2.11/*
+
+    # clean up flink logs
+    rm -f $FLINK_DIR/logs/*
+}
